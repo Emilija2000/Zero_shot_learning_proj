fixed normalization

dataPrep/image_dataset.py
@@ -1,20 +1,29 @@
 import pickle
 import torch
+import torch.nn as nn
 from torch.utils.data import Dataset
 
 class ImageDataset(Dataset):
     # load the dataset
-    def __init__(self, path_data, path_labels,img_size=32,ch=3):
-        
+    def __init__(self, path_data, path_labels, transf_wh, transf_m, ch = 3, img_size=32, patch_size=6, stride=1, padding=0):
+
         with open(path_data, 'rb') as f:
             data = pickle.load(f, encoding='bytes')
 
-        data = data.reshape((-1,img_size,img_size,ch))
-        data = torch.from_numpy(data)
-        self.data = data.permute(0,3,1,2)
+        data = data.reshape((-1,ch,img_size,img_size))
+        self.data = torch.from_numpy(data)
 
         with open(path_labels, 'rb') as f:
             self.labels = pickle.load(f, encoding='bytes')
+
+        self.patch_size=patch_size
+        self.img_size=img_size
+        self.ch = ch
+        self.stride=stride
+        self.padding = padding
+
+        self.transf = torch.from_numpy(transf_wh).float()
+        self.transf_m = torch.Tensor(transf_m).float()
         
     # number of rows in the dataset
     def __len__(self):
@@ -22,4 +31,28 @@ class ImageDataset(Dataset):
  
     # get a row at an index
     def __getitem__(self, idx):
-        return [self.data[idx,:,:,:], self.labels[idx]]
+        # divide into patches
+        img = self.data[idx,:,:,:]
+        
+        if (self.padding>0):
+            pad_dim = [self.padding, self.padding, self.padding, self.padding]
+            img = nn.functional.pad(img, pad_dim, 'constant',0)
+
+        patches = torch.zeros(((self.img_size+2*self.padding-self.patch_size+1)**2, self.ch*self.patch_size**2))
+        i=0
+        for x in range(0,img.shape[1]-self.patch_size+1, self.stride):
+            for y in range(0,img.shape[2]-self.patch_size+1, self.stride):
+                patches[i,:] = img[:,x:x+self.patch_size,y:y+self.patch_size].flatten()  
+                i = i+1
+        
+        # normalize individual patches
+        num_patches = patches.shape[0]
+        std = torch.std(patches,1).reshape(num_patches,1)
+        std[std==0] = 1
+        patches = torch.divide((patches - torch.mean(patches,1).reshape(num_patches,1)),std)
+
+        # zca whitening of image patches
+        patches = torch.matmul(patches-self.transf_m,self.transf)
+
+        return [patches, self.labels[idx]]
+        

dataPrep/image_emb_extract.py
@@ -1,12 +1,10 @@
 import json
 import pickle
-import time
 import torch
 from torch.utils.data import DataLoader
 
 from image_dataset import ImageDataset
 from image_emb_model import OmpT
-
 
 CONFIG_FILE_NAME = 'config.json'
  
@@ -24,49 +22,53 @@ if __name__ == '__main__':
     pooling = config['IMAGE_EMB']['encoders']['omp1-t']['pooling']
     img_size = config['DATASET']['imgs_size']
 
+    out_img_size = 1+(img_size-patch_size+2*padding)//stride
 
-    # load pretrained model parameters and init model
-    file_name = config['DATASET']['imgs_path']+"\\dict.pkl"
-    model = OmpT(in_channels=in_channels,
-        out_channels=out_channels,
-        patch_size=patch_size,
+    # load pretrained model parameters 
+    file_path = config['DATASET']['imgs_path']+"\\dict.pkl"
+    with open(file_path, 'rb') as f:
+        weights,transf_m,transf_wh = pickle.load(f, encoding='bytes')       
+
+
+    # init model
+    model = OmpT(out_channels=out_channels,
+        out_img_size=out_img_size,
         pretrained=True,
-        weights_path=file_name,
+        weights = weights,
         alpha=alpha,
-        stride=stride,
-        padding=padding,
         pool_param=pooling)
 
 
     # read training data
-    file_name_data = config['DATASET']['imgs_path']+"\\train_data.pkl"
-    file_name_labels = config['DATASET']['imgs_path']+"\\train_labels.pkl"
-    b_size=8 #RAM limited
-    dataset = ImageDataset(file_name_data,file_name_labels,img_size,in_channels)
+    file_path_data = config['DATASET']['imgs_path']+"\\train_data.pkl"
+    file_path_labels = config['DATASET']['imgs_path']+"\\train_labels.pkl"
+    
+    b_size=4 #RAM limit
+    dataset = ImageDataset(file_path_data, file_path_labels, transf_wh, transf_m, in_channels, img_size, patch_size, stride, padding)
     dataloader = DataLoader(dataset, batch_size=b_size, shuffle=False)
 
     embeddings = []
 
-    curr_data_batch = 1
-    data_batch_size = dataset.__len__()/5
-    print('Extracting image embeddings...')
-    for i,(data,labels) in enumerate(dataloader):
-        if(i<(curr_data_batch-1)*data_batch_size):
-            continue
-        if(i>=curr_data_batch*data_batch_size):
-            break
+    for j in range(5):
+        curr_data_batch = j
+        data_batch_size = dataset.__len__()/5
+        print('Extracting image embeddings...')
+        for i,(data,labels) in enumerate(dataloader):
+            if(i<(curr_data_batch-1)*data_batch_size):
+                continue
+            if(i>=curr_data_batch*data_batch_size):
+                break
 
-        if(i%1000==0):
-            print("Extracted {}/{}".format(i-(curr_data_batch-1)*data_batch_size,data_batch_size))
+            if(i%1000==0):
+                print("Extracted {}/{}".format(i-(curr_data_batch-1)*data_batch_size,data_batch_size))
 
-        out=model(data)
-        if(b_size>1):
-            embeddings = embeddings+out.tolist() 
-        else:
-            embeddings.append(out.tolist())
-    
-    file_path = config['DATASET']["imgs_ftrs_path"]+"\\image_embs_omp1t_batch"+str(curr_data_batch)+".pkl"
-    with open(file_path, 'wb') as f:
-        pickle.dump(embeddings, f)
-    
+            out=model(data)
+            if(b_size>1):
+                embeddings = embeddings+out.tolist() 
+            else:
+                embeddings.append(out.tolist())
+        
+        file_path = config['DATASET']["imgs_ftrs_path"]+"\\image_embs_omp1t_batch"+str(curr_data_batch)+".pkl"
+        with open(file_path, 'wb') as f:
+            pickle.dump(embeddings, f)
         

dataPrep/image_emb_model.py
@@ -1,30 +1,23 @@
-from numpy import pad
-import pickle
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class OmpT(nn.Module):
 
-    def __init__(self, in_channels=3, out_channels=1600, patch_size=6, pretrained=False, weights_path=None, alpha=0.25, stride=1, padding=0, pool_param=8):
+    def __init__(self, out_channels=1600, out_img_size = 27, pretrained=False, weights=[], alpha=0.25, pool_param=8):
         super(OmpT, self).__init__()
 
-        self.conv = nn.Conv2d(in_channels, out_channels, patch_size, stride=stride, padding=padding, bias=False)
+        #self.conv = nn.Conv2d(in_channels, out_channels, patch_size, stride=stride, padding=padding, bias=False)
 
         # load pretrained dictionary
         if pretrained:
-            if weights_path!=None:
-                with open(weights_path, 'rb') as f:
-                    weights = pickle.load(f, encoding='bytes')
-                weights = torch.from_numpy(weights.T)
-                weights = weights.reshape([out_channels,patch_size,patch_size,in_channels])
-                weights = weights.permute(0,3,1,2)
+            if len(weights)>0:
+                self.weights = torch.from_numpy(weights).float()
 
-                with torch.no_grad():
-                    self.conv.weight = nn.Parameter(weights)
             else:
                 ('Please provide weights of the pretrained network...')
-
+        self.shape = [out_channels, out_img_size, out_img_size]
+        
         # soft threshold value
         self.activation = nn.Softshrink(alpha)
 
@@ -34,16 +27,24 @@ class OmpT(nn.Module):
  
     def forward(self, X):
         # apply dictionary on image patches
-        X = self.conv(X) 
+        X = torch.matmul(self.weights,X.T)
+
+        # reshape into [out_channels, out_img_size, out_img_size]
+        X = X.reshape(self.shape)
+
         # soft threshold
         X = self.activation(X)
+
         # split positive and negative features
-        ftrs_pos = torch.abs(X)
-        ftrs_neg = -torch.abs(-X)
+        ftrs_pos = X * (X>=0)
+        ftrs_neg = X * (X<0)
+
         # average pooling
         ftrs_pos = self.pool(ftrs_pos)
         ftrs_neg = self.pool(ftrs_neg)
-        # final embedding
+
+
+        # final embedding - return flattened vector
         if(len(X.shape)==3):
             emb = torch.cat((ftrs_pos.flatten(), ftrs_neg.flatten()))
         else:

dataPrep/image_emb_train_omp1.py
@@ -28,10 +28,11 @@ if __name__ == '__main__':
     r = np.random.randint(0, img_size-patch_size+1, num_patches)
     c = np.random.randint(0, img_size-patch_size+1, num_patches)
     
-    imgs = x_train[p_imgs,:].reshape((num_patches, img_size, img_size, 3))
+    imgs = x_train[p_imgs,:].reshape((num_patches, 3, img_size, img_size))
+
     patches = np.zeros((num_patches,patch_size*patch_size*3))
     for i in range(num_patches):
-        patch = imgs[i,r[i]:r[i]+patch_size,c[i]:c[i]+patch_size,:]
+        patch = imgs[i,:,r[i]:r[i]+patch_size,c[i]:c[i]+patch_size]
         patches[i,:] = patch.flatten()
         
     # normalization
@@ -41,10 +42,10 @@ if __name__ == '__main__':
 
     # zca whitening
     s = np.cov(patches.T)
-    m = np.mean(patches.T)
-    ph,l,_ = np.linalg.svd(s)
-    transf = np.dot(ph, np.dot(np.diag(1.0/np.sqrt(l + 1e-10)), ph.T))
-    patches = np.dot(patches, transf)
+    m = np.mean(patches,0)
+    l,ph = np.linalg.eig(s)
+    transf = np.dot(ph, np.dot(np.diag(1.0/np.sqrt(l + 0.1)), ph.T))
+    patches = np.dot(patches-m, transf)
 
 
     # initialize trining parameters
@@ -54,12 +55,13 @@ if __name__ == '__main__':
     batch_size = config['IMAGE_EMB']['encoders']['omp1-t']['batch_size']  
     num_batches = int(np.ceil(num_patches/(1.0*batch_size)))
 
-    dictionary = np.random.randn(patch_size*patch_size*3,ftrs_size) #random init
+    # init dict
+    dictionary = np.random.randn(ftrs_size,patch_size*patch_size*3) #random init
+    # normalize dict
+    dictionary = np.divide(dictionary,np.sqrt(np.sum(dictionary*dictionary,1)+1e-20).reshape(-1,1))
+
 
     for i in range(num_iter):
-        # normalize
-        dictionary = np.divide(dictionary,np.std(dictionary,0)+1e-10)
-
         dict_temp = np.zeros(dictionary.shape)
 
         for j in range(num_batches):
@@ -68,22 +70,23 @@ if __name__ == '__main__':
             data = patches[j*batch_size:min(num_patches, (j+1)*batch_size),:]
             
             # find the most correlated component - omp1
-            corr = np.matmul(dictionary.T,data.T) # correlation of dict columns with data vectors - ftrs_size*batch_size
-            k = np.argmax(corr, 0) # vector (batch_size,)
+            corr = np.matmul(dictionary,data.T) # correlation of dict columns with data vectors - ftrs_size*batch_size
+            k = np.argmax(np.abs(corr), 0) # vector (batch_size,)
             # sparse vectors 
             x_sparse = np.zeros((ftrs_size,batch_size))
             x_sparse[k,np.arange(batch_size)] = corr[k,np.arange(batch_size)]
             
             # update dictionary
-            dict_temp = dict_temp + np.matmul(x_sparse,data).T
+            dict_temp = dict_temp + np.matmul(x_sparse,data)
+
         dictionary = dict_temp
+        dictionary = np.divide(dictionary,np.sqrt(np.sum(dictionary*dictionary,1)+1e-20).reshape(-1,1))
 
         if((i+1)%5==0):
             print('Iteration {}/{}'.format(i+1,num_iter))
-
 
     # save dict
     print('Saving image dictionary...')
     file_name = config['DATASET']['imgs_path']+"\\dict.pkl"
     with open(file_name, 'wb') as f:
-        pickle.dump(dictionary, f)
+        pickle.dump((dictionary, m, transf),f)